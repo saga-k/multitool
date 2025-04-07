6c7a12edb76bf28c7f202abc012b18d58ae0f034
saga-k <sagakrusell+1@gmail.com>
2025-04-07 11:43:37 +0200

    refracoted to use reducer

--- src/components/WcagChecklist/WcagChecklist.tsx
+++ src/components/WcagChecklist/WcagChecklist.tsx
@@ -1,45 +1,70 @@
 import React from 'react'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useReducer } from 'react'
 import './WcagChecklist.module.css'
 
+const initialState = {
+  loading: true,
+  fetchedList: null
+}
+
+function reducer(state, action){
+switch(action.type){
+  case 'FETCH_SUCCESS':
+  return {...state, loading: false, data: action.payload}
+}
+
+}
+
 export default function WcagChecklist() {
+
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  interface listObject{
+    title:string,
+    allChecked: boolean
+    items: checkListItems[]
+  }
+
   interface checkListItems{
     text: string,
     checked: boolean,
     id: string
   }
 
-  interface listObject{
-    section:string,
-    allChecked: boolean
-    items: checkListItems[]
-  }
-
-  const [list, setList] = useState<listObject[]>([])
-
   async function fetchList(){
+
+    try{
     const promise = await fetch('/wcag_checklist.json')
+    if(!promise.ok){
+      throw new Error(`Failed to fetch checklist: ${promise.status}`)
+    }
     const fetchedList = await promise.json()
-    setList(fetchedList)
+    dispatch({type: 'FETCH_SUCCESS', payload: fetchedList})
+    }
+    catch (error){
+      console.error(error)
+    }
   }
 
   useEffect(() => {fetchList(),[]})
 
 
+
+
   return (
     <section>
-      {list && list.map(listObject =>
+      {state.data?.map((title:listObject) =>
         <div className='listTitle'
-        key={listObject.section}>
-          <h4>{listObject.section}</h4>
+        key={title.title}>
+          <h4>{title.title}</h4>
           <ul>
-          {listObject.items && listObject.items.map(item =>
-            <li className='listItem' key={item.id}>
-              <input type='checkbox' value={item.id} onChange={(e) => handleChange(e, listObject)}/>
-              <p>{item.text}</p>
-            </li>)}
+          {title.items.map((item : checkListItems) =>
+          <li className='listItem'
+          key={item.id}>
+          <input type='checkBox' value={item.id}/>
+          {item.text}
+          </li>)}
           </ul>
-
         </div>)}
     </section>
   )
@@ -53,5 +78,14 @@ function handleChange(e:React.ChangeEvent<HTMLInputElement>, listObject: listObj
   console.log(items)
   const found = items.find((item) => item.id === e.target.value)
   console.log('found',found)
-  found.checked = e.target.checked;
+  found.checked = e.target.checked
+
+
+            <ul>
+          {state.data.items && state.data.items.map((item, section, index) =>
+            <li className='listItem' key={item.id}>
+              <input type='checkbox' value={item.id} onChange={(event) => handleChange(event, section, index)}/>
+              <p>{item.text}</p>
+            </li>)}
+          </ul>
 }*/}
